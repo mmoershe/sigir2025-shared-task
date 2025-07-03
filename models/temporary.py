e153e6abac012cea8a0e37e1e5112cc85848e8c3
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-07-03 19:10:01 +0200

    executign model locally scripts

---
+++ models/temporary.py
@@ -0,0 +1,4 @@
+from huggingface_hub import snapshot_download
+
+snapshot_download(repo_id="meta-llama/Llama-3.2-3B", local_dir="./llama-3.2-3b")
+
---
+++ models/test_filter_model.py
@@ -0,0 +1,26 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
+import torch
+
+import os
+
+CURRENT_DIRECTORY: str = os.path.dirname(__file__)
+lora_adapter_dir: str = os.path.join(CURRENT_DIRECTORY, "filter_checkpoint")
+
+# Path or repo ID of your base model
+ssase_model_path = "meta-llama/Llama-3.2-3B"
+
+# Load tokenizer (from LoRA dir or base model)
+tokenizer = AutoTokenizer.from_pretrained(lora_adapter_dir)
+
+# Load base model
+base_model = AutoModelForCausalLM.from_pretrained(base_model_path)
+
+# Load LoRA adapters onto the base model
+model = PeftModel.from_pretrained(base_model, lora_adapter_dir).to("cuda")
+
+# Prompt the model
+prompt = "climate change fires danger prediction Output:"
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+outputs = model.generate(**inputs, max_new_tokens=100)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
